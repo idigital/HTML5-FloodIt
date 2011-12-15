d5bde408579228883d3baab029d270ae27ad056f
Matthew Cory <matthewcory1@gmail.com>
2011-12-15 06:24:03 -0700

    Adding a class to track player stats.  Currently just tracks the current game seed and the number of steps taken.
    Also: having problems with referencing MooTools events -- works fine
    for ControlPanel and GameGrid, but "Implements: Events," doesn't seem to
    matter in PlayerStats for some reason.  Will fix when possible.

---
+++ HTML5 FloodIt/WebContent/js/game/PlayerStats.js
@@ -0,0 +1,53 @@
+var PlayerStats = Class({
+	Implements: Events,
+	gameSeed: null,
+	stepCount: 0,
+	
+	initialize: function(pGameSeed, pInitialStepCount) {
+		this.gameSeed = pGameSeed || null;
+		this.stepCount = pInitialStepCount || 0;
+	},
+	
+	getGameSeed: function() { return this.gameSeed; },
+	setGameSeed: function(pGameSeed) { this.gameSeed = pGameSeed; },
+	
+	getStepCount: function() { return this.stepCount; },
+	setStepCount: function(pStepCount) {
+		if (this.stepCount == pStepCount) {
+			return;
+		}
+		
+		var oldSteps = this.stepCount;
+		
+		this.stepCount = pStepCount;
+
+		var newSteps = this.stepCount;
+		
+		var evtArgs = new StepCountChangedEventArgs(oldSteps, newSteps);
+		this.fireEvent("onStepCountChanged", evtArgs);
+	},
+	
+	incrementStepCount: function() { 
+		var oldSteps = this.stepCount;
+		
+		this.stepCount += 1;
+
+		var newSteps = this.stepCount;
+		
+		var evtArgs = new StepCountChangedEventArgs(oldSteps, newSteps);
+		this.fireEvent("onStepCountChanged", evtArgs);
+	}
+});
+
+var StepCountChangedEventArgs = Class({
+	oldStepCount: null,
+	newStepCount: null,
+	
+	initialize: function(pOldStepCount, pNewStepCount) {
+		this.oldStepCount = pOldStepCount;
+		this.newStepCount = pNewStepCount;
+	},
+	
+	getOldStepCount: function() { return this.oldStepCount; },
+	getNewStepCount: function() { return this.newStepCount; }
+});
